0db79861864112b56a061911b540b1a9fdd9d37d
MOHAN <tsmohan00@gmail.com>
2026-06-24 20:19:31 +0530

    config: configure production Render base URLs dynamically

--- credit-underwriter-ui/src/app/services/loan.service.ts
+++ credit-underwriter-ui/src/app/services/loan.service.ts
@@ -7,8 +7,12 @@ import { LoanRequest, LoanResponse } from '../models';
   providedIn: 'root'
 })
 export class LoanService {
-  private loanUrl = 'http://localhost:8081/api/loans';
-  private underwritingUrl = 'http://localhost:8082/api/underwriting/advise';
+  private loanUrl = window.location.hostname === 'localhost'
+    ? 'http://localhost:8081/api/loans'
+    : 'https://loan-service-cx3o.onrender.com/api/loans';
+  private underwritingUrl = window.location.hostname === 'localhost'
+    ? 'http://localhost:8082/api/underwriting/advise'
+    : 'https://underwriting-service.onrender.com/api/underwriting/advise';
 
   constructor(private http: HttpClient) {}
 
